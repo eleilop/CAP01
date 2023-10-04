5bfe3c49cbb854a34e106b6c12037a940d5fb19a
eleilop <118524344+eleilop@users.noreply.github.com>
2023-10-04 12:53:56 +0200

    Add files via upload

---
+++ EJ03/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("%-8s\t%s%n", "computer","ordenador");
+        System.out.printf("%-8s\t%s%n", "student","alumno");
+        System.out.printf("%-8s\t%s%n", "cat","gato");
+        System.out.printf("%-8s\t%s%n", "penguin","pingüino");
+        System.out.printf("%-8s\t%s%n", "machine","máquina");
+        System.out.printf("%-8s\t%s%n", "nature","naturaleza");
+        System.out.printf("%-8s\t%s%n", "light","luz");
+        System.out.printf("%-8s\t%s%n", "green","verde");
+        System.out.printf("%-8s\t%s%n", "book","libro");
+        System.out.printf("%-8s\t%s%n", "pyramid","pirámide");
+    }
+}
