b833be95149dfb5b75ed3582e876219f81bb8ff4
eleilop <118524344+eleilop@users.noreply.github.com>
2023-10-04 12:24:16 +0200

    Add files via upload

---
+++ EJ02/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Eloy Leiva López");
+        System.out.println("Mi dirección es C/ NoEsPu Blico");
+        System.out.println("Mi teléfono es 123456789");
+    }
+}
