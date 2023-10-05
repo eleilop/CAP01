0de86cdc00cb1c6f59acc6302b379bb4f29580cb
Eloy Leiva López <118524344+eleilop@users.noreply.github.com>
2023-10-05 10:08:15 +0200

    Add files via upload

---
+++ EJ04/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "Lunes","Martes","Miérc.","Jueves","Viernes");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "=====","======","======","======","=======");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "PROG","LM","LM","PROG","FOL");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "PROG","LM","LM","PROG","FOL");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "PROG","BD","PROG","PROG","FOL");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "SI","BD","PROG","BD","SI");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "SI","EN","BD","BD","SI");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "SI","EN","BD","EN","SI");
+    }
+}
